e373f74e5a9b367c4f7e1b53d4f2efb3ea1be451
kazackovadara5@gmail.com <KazachkovaDaria>
2026-05-12 00:11:02 +0300

    new breach, something added

---
+++ KazachkovaDA/Task9/main.cpp
@@ -0,0 +1,32 @@
+#include "list.hpp"
+#include "additional.hpp"
+
+int main() {
+
+	const std::string file_name = "list.txt";
+	phrase_library quotes(file_name);
+	phrase_library founded;
+
+	std::string request_word;
+	const std::string exit = "exit";
+
+	do {
+		std::cout << "\nEnter the key word: ";
+		std::cin >> request_word;
+
+		if (request_word == exit)
+		{
+			return 0;
+		}
+		try {
+			quotes.find_quotes_by_key_words(request_word, founded);
+		}
+		catch (const std::exception& ex) {
+			std::cout << ex.what() << std::endl;
+			continue;
+		}
+		std::cout << founded;
+
+	} while (1);
+
+}
